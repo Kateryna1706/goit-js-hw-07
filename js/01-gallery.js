5b5781dff894112531553ca1ed127f0cf08a0ff8
Kateryna1706 <silckatya@gmail.com>
2023-04-13 12:14:54 +0200

    did 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function makeElements(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+    )
+    .join("");
+}
+
+const newElements = makeElements(galleryItems);
+
+gallery.insertAdjacentHTML("afterbegin", newElements);
+
+let instance;
+
+gallery.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  instance = basicLightbox.create(`
+<img src="${event.target.dataset.source}">
+`);
+
+  instance.show();
+
+  document.addEventListener("keydown", handleKeydown);
+}
+
+function handleKeydown(event) {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+}
